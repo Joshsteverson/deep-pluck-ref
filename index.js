bc3febe12ed13c44ca0f57f14d805bac10c4ea6d
JoshSteverson <joshsteverson@gmail.com>
2016-11-16 08:26:17 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function deepPluckRef(obj, findProps) {
+	if(!obj) return undefined;
+	if(!findProps) return undefined;
+	if(typeof findProps.forEach !== 'function') return undefined;
+	var results = [];
+	function recurse(o) {
+		var ref = {ref: o};
+		Object.keys(o).forEach(function keysForEachCb(m) {
+			~findProps.indexOf(m) && !~results.indexOf(ref) && results.push(ref);
+			typeof o[m] === 'object' && recurse(o[m]);
+		});
+	}
+	recurse(obj);
+	return results;
+}
+
+module.exports = deepPluckRef;
---
+++ test/index.js
@@ -0,0 +1,72 @@
+
+'use strict';
+
+require('chai').should();
+var deepPluckRef = require('../index');
+
+
+describe('Module tests', function describeCb() {
+
+	it('plucks values within array', function itCb(done) {
+		var obj = {
+			do: {
+				all: {
+					the: {
+						things: [
+							{
+								thing: 'one',
+								value: 'thing one value'
+							},
+							{
+								thing: 'two',
+								value: 'thing two value'
+							},
+							{
+								thing: 'three'
+							}
+						]
+					}
+				}
+			}
+		};
+		var results = deepPluckRef(obj, ['value']);
+		results.length.should.equal(2);
+		done();
+	});
+
+	it('returns undefined when obj param is null.', function itCb(done) {
+		var obj = null;
+		var results = deepPluckRef(obj, ['prop']);
+		(typeof results).should.equal('undefined');
+		done();
+	});
+
+	it('returns undefined when obj param is undefined.', function itCb(done) {
+		var obj = undefined;
+		var results = deepPluckRef(obj, ['prop']);
+		(typeof results).should.equal('undefined');
+		done();
+	});
+
+	it('returns undefined when obj param is string.', function itCb(done) {
+		var obj = '';
+		var results = deepPluckRef(obj, ['prop']);
+		(typeof results).should.equal('undefined');
+		done();
+	});
+
+	it('returns undefined when findProps param is string.', function itCb(done) {
+		var obj = {};
+		var results = deepPluckRef(obj, 'prop');
+		(typeof results).should.equal('undefined');
+		done();
+	});
+
+	it('returns undefined when findProps param is not array.', function itCb(done) {
+		var obj = {};
+		var results = deepPluckRef(obj, undefined);
+		(typeof results).should.equal('undefined');
+		done();
+	});
+
+});
